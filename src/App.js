6c541d83fbb785ed929867cf9025772caea4817e
Aleenaa <aleenaadavis123@gmail.com>
2023-05-22 15:01:12 +0530

    react with image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Body from "./pages/Body";
 import Footer from "./pages/Footer";
 import Header from "./pages/Header";
+import ImageDemo from "./pages/ImageDemo";
 
 function App() {
   return (
     <div>
       <Header />
       <Body />
+      <ImageDemo/>
       <Body />
       <Body />
       <Body />
